4a02c2403f62c6b9d44dbb7c739b9fd5f60b3197
medbb3 <41924835+medbb3@users.noreply.github.com>
2019-01-22 08:57:31 +0100

    Update StagiaireDao.java

--- projet-jdbc/src/main/java/fr/m2iformation/jdbc/StagiaireDao.java
+++ projet-jdbc/src/main/java/fr/m2iformation/jdbc/StagiaireDao.java
@@ -4,7 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class StagiaireDao extends Dao {
+public class StagiaireDao extends MaConnection {
 
 	static public void insert(Stagiaire stg) {
 
